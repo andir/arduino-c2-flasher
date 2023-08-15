aeaa7f2d13461f980fc30043fd04bce56974d7c2
Chris L <stylesuxx@gmail.com>
2023-08-16 00:23:25 +0200

    Add OG image #2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,28 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Arduino C2 Interface',
-  description: 'Web bases Arduino C2 interface for flashing and reading SiLabs EFM8 based MCUs',
-}
+  description: 'Web bases Arduino C2 interface for flashing, reading and erasing SiLabs EFM8 based MCUs',
+  openGraph: {
+    images: [
+      {
+        url: '/og.png',
+        width: 1200,
+        height: 630,
+      },
+    ]
+  }
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-100">
-      <body className="d-flex flex-column h-100">{children}</body>
+      <body className="d-flex flex-column h-100">
+        {children}
+      </body>
     </html>
-  )
+  );
 }
